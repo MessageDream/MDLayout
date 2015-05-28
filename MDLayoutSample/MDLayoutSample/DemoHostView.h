d28a3a6c59421f1f419d7bd51dae64c6d2f3c54b
Jayden Zhao <jaydenzhao@outlook.com>
2015-05-28 11:11:28 +0800

    testView

---
+++ MDLayoutSample/MDLayoutSample/DemoHostView.h
@@ -0,0 +1,13 @@
+//
+//  DemoHostView.h
+//  MDLayoutSample
+//
+//  Created by Jayden Zhao on 15/5/28.
+//  Copyright (c) 2015年 jayden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoHostView : UIView
+@property(nonatomic,strong)UIButton *headerButton;
+@end
---
+++ MDLayoutSample/MDLayoutSample/DemoHostView.m
@@ -0,0 +1,16 @@
+//
+//  DemoHostView.m
+//  MDLayoutSample
+//
+//  Created by Jayden Zhao on 15/5/28.
+//  Copyright (c) 2015年 jayden. All rights reserved.
+//
+
+#import "DemoHostView.h"
+
+@implementation DemoHostView
+
+-(void)headerButton_click:(id)sender{
+    NSLog(@"clicked:%@",sender);
+}
+@end
